d3c9c605ba9129da1f34a8fe02eda3912b6343fd
Carl Padilla <carlpadilla@protonmail.com>
2020-05-14 19:25:22 -0400

    saving progress on player.py

--- src/player.py
+++ src/player.py
@@ -1,8 +1,21 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
 
-class Player:
-    def __init__(self, name, room):
 
+class Player:
+    '''
+    defining the initial class of Player with player name and room where they start
+    '''
+
+    def __init__(self, name, room):
         self.name = name
-        self.room = room
+        self.current_room = room
+    # How the player moves
+
+    def travel(self, direction):
+        next_room = getattr(self.current_room, f"{direction}_to")
+        if next_room is not None:
+            self.current_room = next_room
+            print(f"You are currently in: \n{self.current_room}")
+        else:
+            print("You can't go in that direction.")
